41b97565e90d24cc3b94ef6ea1b7bb8c5987000e
Leurok <v_barchenko2003@mail.ru>
2026-04-20 14:51:32 +0300

    dimensions

---
+++ app/src/main/java/com/example/flat_rent_app/presentation/theme/TextSizes.kt
@@ -0,0 +1,19 @@
+package com.example.flat_rent_app.presentation.theme
+
+import androidx.compose.ui.unit.sp
+
+object TextSizes {
+    val sp0 = 0.sp
+    val sp0_5 = 0.5.sp
+    val sp10 = 10.sp
+    val sp11 = 11.sp
+    val sp14 = 14.sp
+    val sp15 = 15.sp
+    val sp16 = 16.sp
+    val sp18 = 18.sp
+    val sp22 = 22.sp
+    val sp24 = 24.sp
+    val sp28 = 28.sp
+    val sp36 = 36.sp
+    val sp96 = 96.sp
+}
